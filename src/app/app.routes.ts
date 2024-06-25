62882582d81476559a53646ed25c0d86e767e111
Musdate <bastiantroncoso97@gmail.com>
2024-06-25 09:38:03 -0500

    [FIX] Hash in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
